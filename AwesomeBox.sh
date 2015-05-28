9ad32f6cac0dad75964ca8d8c067c6f33c8120ff
InvertedAcceleration <chris.magee@velocity42.com>
2015-05-28 11:21:40 +0000

    Default values must correctly take the place of empty user input.

--- AwesomeBox.sh
+++ AwesomeBox.sh
@@ -22,19 +22,33 @@ then
     DEFAULT_GIT_EMAIL="`grep 'email = ' ~/.gituserconfig | awk -F= '{print $2};' | tr -d '[[:space:]]'`"
 fi
 
-echo -n -e "\e[0;32m • Enter your git username [${DEFAULT_GIT_USERNAME}]: "
+echo -n -e "\e[0;32m • Enter your git username [$DEFAULT_GIT_USERNAME]: "
 read GIT_USERNAME
 
-[ -z $GIT_USERNAME ] && [ -z $DEFAULT_GIT_USERNAME ] && GIT_USERNAME=$DEFAULT_GIT_USERNAME
-
-echo -n -e "\e[0;32m • Enter your git email [${DEFAULT_GIT_EMAIL}]: "
+echo -n -e "\e[0;32m • Enter your git email [$DEFAULT_GIT_EMAIL]: "
 read GIT_EMAIL
 
-[ -z $GIT_EMAIL ] && [ -z $DEFAULT_GIT_EMAIL ] && GIT_EMAIL=$DEFAULT_GIT_EMAIL
+[ -z $GIT_USERNAME ] && [ ! -z $DEFAULT_GIT_USERNAME ] && GIT_USERNAME=$DEFAULT_GIT_USERNAME
+[ -z $GIT_EMAIL ] && [ ! -z $DEFAULT_GIT_EMAIL ] && GIT_EMAIL=$DEFAULT_GIT_EMAIL
+
+if [ -z $GIT_USERNAME ] || [ -z $GIT_EMAIL ]
+then
+    echo ""
+    echo -e "\e[0;31m Aborting - an Awesome Box requires a git username and git email. :(\033[0m"
+    echo ""
+    exit 5
+fi
+
+echo ""
+
+echo -e "\e[0;34m • Updating git user config.\033[0m"
+
+cp ~/AwesomeBox/dotFiles/.gituserconfig ~
+sed -i "s/GIT_USERNAME/$GIT_USERNAME/" ~/.gituserconfig
+sed -i "s/GIT_EMAIL/$GIT_EMAIL/" ~/.gituserconfig
 
 echo -e "\e[0;34m • Installing core tools.\033[0m"
 
-sudo apt-get update > /dev/null
 sudo apt-get -q -y install python-software-properties > /dev/null
 sudo add-apt-repository -y ppa:git-core/ppa > /dev/null 2>&1
 
@@ -60,23 +74,11 @@ do
     ln -s ~/AwesomeBox/dotFiles/$DOT_FILE ~
 done
 
-DOT_FILES_TO_COPY=('.gituserconfig')
-
-for DOT_FILE in ${DOT_FILES_TO_COPY[@]}
-do
-    cp -n ~/AwesomeBox/dotFiles/$DOT_FILE ~
-done
-
 echo -e "\e[0;34m • Installing vundle.\033[0m"
 git clone -q https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null 2>&1
 
 echo -e "\e[0;34m • Installing vim plugins.\033[0m"
-vim +PluginInstall +qall
-
-echo -e "\e[0;34m • Updating local config files.\033[0m"
-
-sed -i "s/GIT_USERNAME/$GIT_USERNAME/" ~/.gituserconfig
-sed -i "s/GIT_EMAIL/$GIT_EMAIL/" ~/.gituserconfig
+vim +PluginUpdate +qall
 
 cat ~/.bashrc | grep -v "source ~/.bashrc_extras" | tee ~/.bashrc > /dev/null
 echo "source ~/.bashrc_extras" | tee -a ~/.bashrc > /dev/null
